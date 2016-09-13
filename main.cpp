b4456224ff6aeea7e396adb5451cd73f8245bac8
Chaoyang Liu <chaoyanglius@gmail.com>
2016-09-13 15:53:24 +0800

    improve the codes

--- main.cpp
+++ main.cpp
@@ -8,9 +8,9 @@ using namespace std;
 int main()
 {
     int k = N;
-    int sche = pow(2, k);
+    int sche = pow(2, k);      // divide the problem to pow(2, k) subproblems
     int arr[sche][sche];
-
+    // inits the array with 0 and gives 1 to 8 to line 1
     for (int i = 0; i < sche; i++)
     {
         for (int j = 0; j < sche; j++)
@@ -27,19 +27,22 @@ int main()
     }
     for (int j = 0; j < k; j++)
     {
+        // gets the size of the problem,
+        // every loop the problem will triple
         int m_size = pow(2, j);
         for (int r = 0; r < pow(2, j); r++)
         {
             for (int c = 0; c < sche; c++)
             {
-                int m = (c + m_size) / m_size;
-
-                int c_offset = pow(-1, m + 1) * m_size;
+                // uses round to get the index of problem block
+                int bid = (c + m_size) / m_size;
+                int c_offset = pow(-1, bid + 1) * m_size;
                 int r_offset = m_size;
                 arr[r + r_offset][c + c_offset] = arr[r][c];
             }
         }
     }
+    // outputs the schedule of the round robin
     for (int i = 0; i < sche; i++)
     {
         for (int j = 0; j < sche; j++)
