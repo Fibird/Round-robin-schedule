bb3088bdc26bae39b1df2eaf70e61c4e9ec043d9
Chaoyang Liu <chaoyanglius@gmail.com>
2016-09-14 11:36:33 +0800

    improve the codes

--- main.cpp
+++ main.cpp
@@ -7,42 +7,37 @@ using namespace std;
 
 int main()
 {
-    int k = N;
-    int sche = pow(2.0, k);      // divide the problem to pow(2, k) subproblems
+    int sche = pow(2.0, N);      // divide the problem to pow(2, k) subproblems
     int **arr;
+    int m_size, bid;
+    int c_offset, r_offset;
+
+    // allocate memory for array
     arr = new int*[sche];
     for (int i = 0; i < sche; i++)
     {
         arr[i] = new int[sche];
     }
-    // inits the array with 0 and gives 1 to 8 to line 1
+
+    // inits line 1 with 1 to 8
     for (int i = 0; i < sche; i++)
     {
-        for (int j = 0; j < sche; j++)
-        {
-            if (i == 0)
-            {
-                arr[i][j] = j + 1;
-            }
-            else
-            {
-                arr[i][j] = 0;
-            }
-        }
+        arr[0][i] = i + 1;
     }
-    for (int j = 0; j < k; j++)
+
+    for (int j = 0; j < N; j++)
     {
         // gets the size of the problem,
         // every loop the problem will triple
-        int m_size = pow(2.0, j);
+        m_size = pow(2.0, j);
         for (int r = 0; r < m_size; r++)
         {
             for (int c = 0; c < sche; c++)
             {
                 // uses round to get the index of problem block
-                int bid = (c + m_size) / m_size;
-                int c_offset = pow(-1.0, bid + 1) * m_size;
-                int r_offset = m_size;
+                bid = (c + m_size) / m_size;
+                c_offset = pow(-1.0, bid + 1) * m_size;
+                r_offset = m_size;
                 arr[r + r_offset][c + c_offset] = arr[r][c];
             }
         }
